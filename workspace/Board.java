09761d3e39dbe722ba3697fc0b7bd5b3fa616c96
SB1809 <sophia.babayev@gmail.com>
2025-03-05 16:45:28 +0000

    c

--- workspace/Board.java
+++ workspace/Board.java
@@ -69,8 +69,9 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
                     board[rows][cols]= new Square(this, true, rows, cols);
                 } else 
                 {
-
+                    board[rows][cols]= new Square(this, false, rows, cols);
                 }
+                this.add(board[rows][cols]);
             }
         }  
 
@@ -93,8 +94,44 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 	//since we only have one kind of piece for now you need only set the same number of pieces on either side.
 	//it's up to you how you wish to arrange your pieces.
     private void initializePieces() {
+
+        board[0][0].put(new Piece(false, RESOURCES_BROOK_PNG));
+        board[0][1].put(new Piece(false, RESOURCES_BKNIGHT_PNG));
+        board[0][2].put(new Piece(false, RESOURCES_BBISHOP_PNG));
+        board[0][3].put(new Piece(false, RESOURCES_BQUEEN_PNG));
+        board[0][4].put(new Piece(false, RESOURCES_BKING_PNG));
+        board[0][5].put(new Piece(false, RESOURCES_BBISHOP_PNG));
+        board[0][6].put(new Piece(false, RESOURCES_BKNIGHT_PNG));
+        board[0][7].put(new Piece(false, RESOURCES_BROOK_PNG));
+
+        board[1][0].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][1].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][2].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][3].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][4].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][5].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][6].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        board[1][7].put(new Piece(false, RESOURCES_BPAWN_PNG));
+
+
     	
-    	board[0][0].put(new Piece(true, RESOURCES_WKING_PNG));
+    	board[7][0].put(new Piece(true, RESOURCES_WROOK_PNG));
+        board[7][1].put(new Piece(true, RESOURCES_WKNIGHT_PNG));
+        board[7][2].put(new Piece(true, RESOURCES_WBISHOP_PNG));
+        board[7][3].put(new Piece(true, RESOURCES_WQUEEN_PNG));
+        board[7][4].put(new Piece(true, RESOURCES_WKING_PNG));
+        board[7][5].put(new Piece(true, RESOURCES_WBISHOP_PNG));
+        board[7][6].put(new Piece(true, RESOURCES_WKNIGHT_PNG));
+        board[7][7].put(new Piece(true, RESOURCES_WROOK_PNG));
+
+        board[6][0].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][1].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][2].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][3].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][4].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][5].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][6].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        board[6][7].put(new Piece(true, RESOURCES_WPAWN_PNG));
 
     }
 
@@ -163,7 +200,13 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     public void mouseReleased(MouseEvent e) {
         Square endSquare = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
         
-        //using currPiece
+            for(Square [] row: board) {
+        	    for(Square s: row) {
+        		s.setBorder(null);
+     
+        	    }
+        	
+            }
         
        
         fromMoveSquare.setDisplay(true);
@@ -171,11 +214,21 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         repaint();
     }
 
+
     @Override
+    //mouse dragged is called when the user is holding the piece and moving the mouse
     public void mouseDragged(MouseEvent e) {
         currX = e.getX() - 24;
         currY = e.getY() - 24;
-
+    
+    // let's highligh all the squares that are legal to move to!
+        if(currPiece!= null) {
+            for(Square s: currPiece.getLegalMoves(this, fromMoveSquare)) {
+            s.setBorder(BorderFactory.createLineBorder(Color.MAGENTA));
+            }
+        
+        }
+    
         repaint();
     }
 
