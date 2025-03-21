c81f7a774bfa2a6991a4bf4ee9354324acf2cc4b
SB1809 <sophia.babayev@gmail.com>
2025-03-21 18:52:35 +0000

    C

--- workspace/Piece.java
+++ workspace/Piece.java
@@ -46,22 +46,11 @@ public class Piece {
     }
     
     
-
+    //Pre- The inputs cant be null and the square referred to must be in bounds of the board.
+    //Post- Returns what moves are possible for the peice to defend against.
     public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
 
       ArrayList<Square> moves = new ArrayList<Square>();
-
-      /*if (!color && (start.getRow()+1<8)){
-
-          moves.add(board[start.getRow()+1][start.getCol()]);
-      }
-
-
-      if (color && (start.getRow()+1<8)){
-
-          moves.add(board[start.getRow()-1][start.getCol()]);
-      }
-*/
 
       if ((start.getRow()+1<8) && (start.getCol()+1<8)){
    
@@ -89,12 +78,8 @@ public class Piece {
     }
     
 
-    //TO BE IMPLEMENTED!
-    //implement the move function here
-    //it's up to you how the piece moves, but at the very least the rules should be logical and it should never move off the board!
-    //returns an arraylist of squares which are legal to move to
-    //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
-    //going to score any points.
+    //Pre- The inputs cant be null and the square referred to must be in bounds of the board.Pre-
+    //Post- Returns what moves are possible for the Piece to move to.
     public ArrayList<Square> getLegalMoves(Board b, Square start){
       //start by declaring the arrayList we wish to return at the end
       ArrayList<Square> moves = new ArrayList<Square>();
@@ -102,9 +87,6 @@ public class Piece {
       Square s = null;
       Square sl = null;
     
-      /*make sure the squares we are adding are in bounds. 
-      (you’d also need to check to make sure they aren’t occupied by a piece unless it’s a piece of the opposite color in which case you can capture it.
-      I leave this bit for you to figure out.*/
       if (!color && (start.getRow()+1<8)){
 
         s = b.getSquareArray()[start.getRow()+1][start.getCol()];
